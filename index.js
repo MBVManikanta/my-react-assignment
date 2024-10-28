dfcded374bc9e420d8bafe14271b50112efcf963
MBVManikanta <152022290+MBVManikanta@users.noreply.github.com>
2024-10-28 13:05:33 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './styles.css'; // Ensure this file exists in the src folder
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+// import './styles.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App />);
+
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import './styles.css';
+import App from './App';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App/>
+  </StrictMode>,
+)
